c863d6be7d42097b3d2ef6681a540850ae7b8f70
Muhammad umer <umershaikh217@gmail.com>
2023-08-24 19:40:55 +0500

    rescource allocation

--- Extras/roundRobin.js
+++ Extras/roundRobin.js
@@ -1,139 +1,149 @@
- 
-// A class to represent a Process Control Block (PCB)
 class PCB {
-    constructor(processId, arrivalTime, executionTime) {
-      this.processId = processId;
-      this.arrivalTime = arrivalTime;
-      this.executionTime = executionTime;
-      this.quantumSize = 3; // Set quantum size
-      this.remainingTime = executionTime;
-      this.resumeInstructionAddress = 0; // Initialize to start
-      this.noOfInstructions = executionTime; // Assume each instruction takes half of execution time
-      this.turnAroundTime = 0;
-      this.utilizationTime = 0;
-      this.psw = 0; // Program Status Word
-      this.schedulingAlgo = 'Round Robin';
-      this.finishTime = 0;
-    }
+  constructor(processId, arrivalTime, executionTime, resourceRequest) {
+    this.processId = processId;
+    this.arrivalTime = arrivalTime;
+    this.executionTime = executionTime;
+    this.resourceRequest = resourceRequest;
+    this.quantumSize = 0;
+    this.remainingTime = executionTime;
+    this.resumeInstructionAddress = 0;
+    this.noOfInstructions = executionTime;
+    this.turnAroundTime = 0;
+    this.utilizationTime = 0;
+    this.schedulingAlgo = 'Round Robin';
+    this.finishTime = 0;
+    this.state = 'Ready'; // Initialize state as Ready
+    this.resourcesAllocated = false; // Initialize resource allocation status
   }
-  
-  // Function to create processes based on user input
-  function createProcesses() {
-    const readline = require('readline');
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-  
-    rl.question('How many processes do you want to create? (3-5): ', (numProcesses) => {
-      numProcesses = parseInt(numProcesses);
-      if (numProcesses < 3 || numProcesses > 5) {
-        console.log('Number of processes must be between 3 and 5.');
-        rl.close();
-        return;
-      }
-  
-      const processes = [];
-      let processCount = 1;
-  
-      function createProcess() {
-        rl.question(`Enter arrival time for Process P${processCount}: `, (arrivalTime) => {
-          arrivalTime = parseInt(arrivalTime);
-          rl.question(`Enter execution time for Process P${processCount} (<= 10): `, (executionTime) => {
-            executionTime = parseInt(executionTime);
-            if (executionTime > 10) {
-              console.log('Execution time must be less than or equal to 10.');
-              rl.close();
-              return;
-            }
-  
-            const process = new PCB(`P${processCount}`, arrivalTime, executionTime);
-            processes.push(process);
-  
-            processCount++;
-            if (processCount <= numProcesses) {
-              createProcess();
-            } else {
-              rl.close();
-              startScheduling(processes);
-            }
+}
+
+function createProcesses() {
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('How many processes do you want to create? (3-5): ', (numProcesses) => {
+    numProcesses = parseInt(numProcesses);
+    if (numProcesses < 3 || numProcesses > 5) {
+      console.log('Number of processes must be between 3 and 5.');
+      rl.close();
+      return;
+    }
+
+    const processes = [];
+    let processCount = 1;
+
+    function createProcess() {
+      rl.question(`Enter arrival time for Process P${processCount}: `, (arrivalTime) => {
+        arrivalTime = parseInt(arrivalTime);
+        rl.question(`Enter execution time for Process P${processCount} (<= 10): `, (executionTime) => {
+          executionTime = parseInt(executionTime);
+          if (executionTime > 10) {
+            console.log('Execution time must be less than or equal to 10.');
+            rl.close();
+            return;
+          }
+
+          rl.question(`Enter resource request for Process P${processCount} (true/false): `, (resourceRequest) => {
+            resourceRequest = resourceRequest.toLowerCase() === 'true';
+
+            rl.question(`Enter quantum size for Process P${processCount} (2-4): `, (quantumSize) => {
+              quantumSize = parseInt(quantumSize);
+              if (quantumSize < 2 || quantumSize > 4) {
+                console.log('Quantum size must be between 2 and 4.');
+                rl.close();
+                return;
+              }
+
+              const process = new PCB(`P${processCount}`, arrivalTime, executionTime, resourceRequest);
+              process.quantumSize = quantumSize;
+              processes.push(process);
+
+              processCount++;
+              if (processCount <= numProcesses) {
+                createProcess();
+              } else {
+                rl.close();
+                startScheduling(processes);
+              }
+            });
           });
         });
-      }
-  
-      createProcess();
-    });
-  }
-  
- 
-  // Function to simulate Round Robin scheduling
-  function startScheduling(processes) {
-    const queue = [...processes];
-    let currentTime = 0;
-  
-    // Sort processes by arrival time before starting the simulation
-    // queue.sort((a, b) => a.arrivalTime - b.arrivalTime);
-    queue.sort((a, b) => {
-        if (a.arrivalTime === b.arrivalTime) {
-          // If arrival times are equal, sort by process ID
-          return a.processId.localeCompare(b.processId);
-        }
-        return a.arrivalTime - b.arrivalTime;
       });
-    
-      // Find the process with the least arrival time
-      const firstProcess = queue.find(process => process.arrivalTime === queue[0].arrivalTime);
-      if (firstProcess) {
-        firstProcess.resumeInstructionAddress = 0;
-      }
-    while (queue.length > 0) {
-      const process = queue.shift();
-      const timeSlice = Math.min(process.remainingTime, process.quantumSize);
-  
-  
-      currentTime += timeSlice;
-      process.utilizationTime += timeSlice;
-      process.remainingTime -= timeSlice;
-      process.resumeInstructionAddress += process.noOfInstructions;
- 
-  
-
-           
-            console.log(`  \n  `);
-            console.log(`Executing ${process.processId} for ${timeSlice} units of time.`);      
-            console.log(`  Process ID: ${process.processId}`);
-            console.log(`  Number of Instructions: ${process.noOfInstructions}`);
-            console.log(`  Arrival Time: ${process.arrivalTime}`);
-            console.log(`  Total Execution Time: ${process.executionTime}`);
-            console.log(`  Remaining Time: ${process.remainingTime}`);
-            console.log(`  Quantum Size: ${process.quantumSize}`);
-            console.log(`  Turnaround Time: ${process.turnAroundTime}`);
-            console.log(`  Utilization Time: ${process.utilizationTime}`);
-          //   console.log(`  PSW: ${process.psw}`);
-            console.log(`  Current Time: ${currentTime} units.`);
-         
-            console.log(`  Resume Instruction Address: ${process.resumeInstructionAddress}`);
-            console.log(`  \n  `);
-
-
-            if (process.remainingTime <= 0) {
-                // Calculate Turnaround Time when process is completed
-                process.finishTime = currentTime;
-                process.turnAroundTime = process.finishTime - process.arrivalTime;
-                console.log(`Finish Time: ${process.finishTime}`);
-                console.log(`${process.processId} has completed execution.`);
-                
-              } else {
-                // Place the process back in the queue
-                queue.push(process);
-              }
-  
     }
-  
-    console.log('All processes have completed execution.');
-  }
-  // Start the simulation by creating processes
-  createProcesses();
 
-  
- 
+    createProcess();
+  });
+}
+
+function startScheduling(processes) {
+  const queue = [...processes];
+  let currentTime = 0;
+
+  queue.sort((a, b) => {
+    if (a.arrivalTime === b.arrivalTime) {
+      return a.processId.localeCompare(b.processId);
+    }
+    return a.arrivalTime - b.arrivalTime;
+  });
+
+  const firstProcess = queue.find(process => process.arrivalTime === queue[0].arrivalTime);
+  if (firstProcess) {
+    firstProcess.resumeInstructionAddress = 0;
+  }
+
+  while (queue.length > 0) {
+    const process = queue.shift();
+    const timeSlice = Math.min(process.remainingTime, process.quantumSize);
+
+    console.log(`\nExecuting ${process.processId} for ${timeSlice} units of time.`);
+
+    if (process.resourceRequest && !process.resourcesAllocated) {
+      console.log(`${process.processId} is in Blocked state due to resource request.`);
+      process.state = 'Blocked';
+    } else {
+      process.state = 'Running';
+      for (let i = 0; i < timeSlice; i++) {
+        if (process.resumeInstructionAddress < process.noOfInstructions) {
+          console.log(`  Executing Instruction: ${process.processId}${String.fromCharCode(65 + process.resumeInstructionAddress)}`);
+          process.resumeInstructionAddress++;
+          currentTime++;
+        }
+      }
+      process.remainingTime -= timeSlice;
+      process.utilizationTime += timeSlice;
+
+
+      console.log(`  \n  `);
+      //   console.log(`  Resume Intruction: ${process.instructionRegister}`);
+      // console.log(`  Instruction Register: ${process.instructionRegister}`);
+        console.log(`  Number of Instructions: ${process.noOfInstructions}`);
+        console.log(`  Process State: ${process.state}`);
+        console.log(`  Quantum Size: ${process.quantumSize}`);
+        console.log(`  Arrival Time: ${process.arrivalTime}`);
+        console.log(`  Total Execution Time: ${process.executionTime}`);
+        console.log(`  Remaining Time: ${process.remainingTime}`);
+        console.log(`  Quantum Size: ${process.quantumSize}`);
+        console.log(`  Turnaround Time: ${process.turnAroundTime}`);
+        console.log(`  Utilization Time: ${process.utilizationTime}`);
+        console.log(`  Current Time: ${currentTime} units.`);
+        console.log(`  \n  `);
+
+        
+      if (process.remainingTime <= 0) {
+        process.finishTime = currentTime;
+        process.turnAroundTime = process.finishTime - process.arrivalTime;
+        console.log(`Finish Time: ${process.finishTime}`);
+        console.log(`${process.processId} has completed execution.`);
+      } else {
+        queue.push(process);
+      }
+    }
+  }
+
+  console.log('All processes have completed execution.');
+}
+
+createProcesses();
--- Final.js
+++ Final.js
@@ -1,54 +1,59 @@
 class PCB {
-    constructor(processId, arrivalTime, executionTime) {
-        this.processId = processId;
-        this.arrivalTime = arrivalTime;
-        this.executionTime = executionTime;
-        this.quantumSize = 0; // Set quantum size
-        this.remainingTime = executionTime;
-        this.resumeInstructionAddress = 0; // Initialize to start
-        this.noOfInstructions = executionTime; // Assume each instruction takes half of execution time
-        this.turnAroundTime = 0;
-        this.utilizationTime = 0;
-        this.psw = 0; // Program Status Word
-        this.schedulingAlgo = 'Round Robin';
-        this.finishTime = 0;
-        this.instructionRegister = ''; // Instruction Register to store the current instruction
-        this.programCounter = 0; // Initialize program counter to 0
-        this.instructions = Array.from({ length: executionTime }, (_, i) => `${processId}${String.fromCharCode(65 + i)}`); // Create an array of instruction names
-        this.state = 'Ready';
-      }
+  constructor(processId, arrivalTime, executionTime, resourceRequest) {
+    this.processId = processId;
+    this.arrivalTime = arrivalTime;
+    this.executionTime = executionTime;
+    this.resourceRequest = resourceRequest;
+    this.quantumSize = 0;
+    this.remainingTime = executionTime;
+    this.resumeInstructionAddress = 0;
+    this.noOfInstructions = executionTime;
+    this.turnAroundTime = 0;
+    this.utilizationTime = 0;
+    this.schedulingAlgo = 'Round Robin';
+    this.finishTime = 0;
+    this.state = 'Ready';
+    this.resourcesAllocated = false;
+    this.instructions = [];
+    for (let i = 0; i < this.noOfInstructions; i++) {
+      this.instructions.push(`${this.processId}${String.fromCharCode(65 + i)}`);
+    }
+    this.programCounter = 0;
   }
-  
-  function createProcesses() {
-    const readline = require('readline');
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-  
-    rl.question('How many processes do you want to create? (3-5): ', (numProcesses) => {
-      numProcesses = parseInt(numProcesses);
-      if (numProcesses < 3 || numProcesses > 5) {
-        console.log('Number of processes must be between 3 and 5.');
-        rl.close();
-        return;
-      }
-  
-      const processes = [];
-      let processCount = 1;
-  
+}
 
-      function createProcess() {
-        rl.question(`Enter arrival time for Process P${processCount}: `, (arrivalTime) => {
-          arrivalTime = parseInt(arrivalTime);
-          rl.question(`Enter execution time for Process P${processCount} (<= 10): `, (executionTime) => {
-            executionTime = parseInt(executionTime);
-            if (executionTime > 10) {
-              console.log('Execution time must be less than or equal to 10.');
-              rl.close();
-              return;
-            }
-  
+function createProcesses() {
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('How many processes do you want to create? (3-5): ', (numProcesses) => {
+    numProcesses = parseInt(numProcesses);
+    if (numProcesses < 3 || numProcesses > 5) {
+      console.log('Number of processes must be between 3 and 5.');
+      rl.close();
+      return;
+    }
+
+    const processes = [];
+    let processCount = 1;
+
+    function createProcess() {
+      rl.question(`Enter arrival time for Process P${processCount}: `, (arrivalTime) => {
+        arrivalTime = parseInt(arrivalTime);
+        rl.question(`Enter execution time for Process P${processCount} (<= 10): `, (executionTime) => {
+          executionTime = parseInt(executionTime);
+          if (executionTime > 10) {
+            console.log('Execution time must be less than or equal to 10.');
+            rl.close();
+            return;
+          }
+
+          rl.question(`Enter resource request for Process P${processCount} (true/false): `, (resourceRequest) => {
+            resourceRequest = resourceRequest.toLowerCase() === 'true';
+
             rl.question(`Enter quantum size for Process P${processCount} (2-4): `, (quantumSize) => {
               quantumSize = parseInt(quantumSize);
               if (quantumSize < 2 || quantumSize > 4) {
@@ -56,11 +61,11 @@ class PCB {
                 rl.close();
                 return;
               }
-  
-              const process = new PCB(`P${processCount}`, arrivalTime, executionTime);
+
+              const process = new PCB(`P${processCount}`, arrivalTime, executionTime, resourceRequest);
               process.quantumSize = quantumSize;
               processes.push(process);
-  
+
               processCount++;
               if (processCount <= numProcesses) {
                 createProcess();
@@ -71,144 +76,90 @@ class PCB {
             });
           });
         });
-      }
-  
-      createProcess();
-    });
-  }
-
-  //     function createProcess() {
-  //       rl.question(`Enter arrival time for Process P${processCount}: `, (arrivalTime) => {
-  //         arrivalTime = parseInt(arrivalTime);
-  //         rl.question(`Enter execution time for Process P${processCount} (<= 10): `, (executionTime) => {
-  //           executionTime = parseInt(executionTime);
-  //           if (executionTime > 10) {
-  //             console.log('Execution time must be less than or equal to 10.');
-  //             rl.close();
-  //             return;
-  //           }
-  
-
-  
-
-  //           const process = new PCB(`P${processCount}`, arrivalTime, executionTime);
-  //           processes.push(process);
-  
-  //           processCount++;
-  //           if (processCount <= numProcesses) {
-  //             createProcess();
-  //           } else {
-  //             rl.close();
-  //             startScheduling(processes);
-  //           }
-  //         });
-  //       });
-  //     }
-  
-  //     createProcess();
-  //   });
-  // }
-  
-  function startScheduling(processes) {
-    const queue = [...processes];
-    let currentTime = 0;
-    queue.sort((a, b) => {
-        if (a.arrivalTime === b.arrivalTime) {
-          // If arrival times are equal, sort by process ID
-          return a.processId.localeCompare(b.processId);
-        }
-        return a.arrivalTime - b.arrivalTime;
       });
-    
-      // Find the process with the least arrival time
-      const firstProcess = queue.find(process => process.arrivalTime === queue[0].arrivalTime);
-      if (firstProcess) {
-        firstProcess.resumeInstructionAddress = 0;
-      }
-    // ... (same as before)
-  
-    while (queue.length > 0) {
-      const process = queue.shift();
-      const timeSlice = Math.min(process.remainingTime, process.quantumSize);
-  
-      console.log(`\nExecuting ${process.processId} for ${timeSlice} units of time.`);
-
-
-      // Inside the loop in startScheduling function
-for (let i = 0; i < timeSlice; i++) {
-    if (process.programCounter < process.instructions.length) {
-        const currentInstructionIndex = process.programCounter;
-        const currentInstruction = process.instructions[currentInstructionIndex];
-        
-        const nextInstructionIndex = process.programCounter + 1;
-        const nextInstruction = nextInstructionIndex < process.instructions.length
-            ? process.instructions[nextInstructionIndex]
-            : 'No more instructions';
-        
-        console.log(` Current Executing Instruction: ${currentInstruction}`);
-        console.log(`  Next Instruction: ${nextInstruction}`);
-        
-        process.programCounter++;
-        currentTime++;
     }
+
+    createProcess();
+  });
 }
 
-  
+function startScheduling(processes) {
+  const queue = [...processes];
+  let currentTime = 0;
+
+  queue.sort((a, b) => {
+    if (a.arrivalTime === b.arrivalTime) {
+      return a.processId.localeCompare(b.processId);
+    }
+    return a.arrivalTime - b.arrivalTime;
+  });
+
+  const firstProcess = queue.find(process => process.arrivalTime === queue[0].arrivalTime);
+  if (firstProcess) {
+    firstProcess.resumeInstructionAddress = 0;
+  }
+
+  while (queue.length > 0) {
+    const process = queue.shift();
+    const timeSlice = Math.min(process.remainingTime, process.quantumSize);
+
+    console.log(`\nExecuting ${process.processId} for ${timeSlice} units of time.`);
+
+    if (process.resourceRequest && !process.resourcesAllocated) {
+      console.log(`${process.processId} is in Blocked state due to resource request.`);
+      process.state = 'Blocked';
+    } else {
+      process.state = 'Running';
+      for (let i = 0; i < timeSlice; i++) {
+        if (process.programCounter < process.instructions.length) {
+          const currentInstructionIndex = process.programCounter;
+          const currentInstruction = process.instructions[currentInstructionIndex];
+          
+          const nextInstructionIndex = process.programCounter + 1;
+          const nextInstruction = nextInstructionIndex < process.instructions.length
+              ? process.instructions[nextInstructionIndex]
+              : 'No more instructions';
+          
+          console.log(` IR current Instruction: ${currentInstruction}`);
+          console.log(` PC Next Instruction: ${nextInstruction}`);
+          
+          process.programCounter++;
+          currentTime++;
+        }
+      }
+
       process.remainingTime -= timeSlice;
       process.utilizationTime += timeSlice;
-  
-      // Output the process information after each time slice
+
+
       console.log(`  \n  `);
-    //   console.log(`  Resume Intruction: ${process.instructionRegister}`);
-    // console.log(`  Instruction Register: ${process.instructionRegister}`);
-      console.log(`  Number of Instructions: ${process.noOfInstructions}`);
-      console.log(`  Process State: ${process.state}`);
-      console.log(`  Quantum Size: ${process.quantumSize}`);
-      console.log(`  Arrival Time: ${process.arrivalTime}`);
-      console.log(`  Total Execution Time: ${process.executionTime}`);
-      console.log(`  Remaining Time: ${process.remainingTime}`);
-      console.log(`  Quantum Size: ${process.quantumSize}`);
-      console.log(`  Turnaround Time: ${process.turnAroundTime}`);
-      console.log(`  Utilization Time: ${process.utilizationTime}`);
-      console.log(`  Current Time: ${currentTime} units.`);
-      console.log(`  \n  `);
-  
+ 
+        console.log(`  Number of Instructions: ${process.noOfInstructions}`);
+        console.log(`  Process State: ${process.state}`);
+        console.log(`  Quantum Size: ${process.quantumSize}`);
+        console.log(`  Arrival Time: ${process.arrivalTime}`);
+        console.log(`  Total Execution Time: ${process.executionTime}`);
+        console.log(`  Remaining Time: ${process.remainingTime}`);
+        console.log(`  Quantum Size: ${process.quantumSize}`);
+        console.log(`  Turnaround Time: ${process.turnAroundTime}`);
+        console.log(`  Utilization Time: ${process.utilizationTime}`);
+        console.log(`  Current Time: ${currentTime} units.`);
+        console.log(`  \n  `);
+
+
+
       if (process.remainingTime <= 0) {
-        // Calculate Turnaround Time when process is completed
         process.finishTime = currentTime;
         process.turnAroundTime = process.finishTime - process.arrivalTime;
         console.log(`Finish Time: ${process.finishTime}`);
         console.log(`${process.processId} has completed execution.`);
       } else {
-        // Place the process back in the queue
         queue.push(process);
       }
     }
-    console.log('All processes have completed execution.');
   }
-  
-  createProcesses();
-  
 
-  
-    //   for (let i = 0; i < timeSlice; i++) {
-    //     if (process.programCounter < process.instructions.length) {
-    //         const instruction = process.instructions[process.programCounter];
-    //         console.log(`  Executing Instruction: ${instruction}`);
-    //         process.programCounter++;
-    //         currentTime++;
-    //     }
-    // }
+  console.log('All processes have completed execution.');
+}
 
-    //   for (let i = 0; i < timeSlice; i++) {
-    //     if (process.resumeInstructionAddress < process.noOfInstructions) {
-    //       // Update the instruction register with the current instruction
-    //     //   const nextInstructionIndex = process.resumeInstructionAddress + 1;
-    //     //   process.instructionRegister = `${process.processId}${String.fromCharCode(65 + nextInstructionIndex)}`;
-    //     //   console.log(`  Executing Instruction: ${process.processId}${String.fromCharCode(65 + process.resumeInstructionAddress)}`);
-    //     process.instructionRegister = `${process.processId}${String.fromCharCode(65 + process.resumeInstructionAddress)}`;
-    //     console.log(`  Executing Instruction: ${process.instructionRegister}`);
-    //     process.resumeInstructionAddress++;
-    //     currentTime++;
-    //     }
-    //   }
+createProcesses();
